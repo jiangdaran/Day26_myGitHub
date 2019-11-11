1d2abe3364ba10baf3249b9937d4843de3ea65b8
jiangdaran <598360175@qq.com>
2019-11-11 15:05:16 +0800

    Initial commit

---
+++ src/com/ran/test/Demo.java
@@ -0,0 +1,11 @@
+package com.ran.test;
+
+/**
+ * @author Ran
+ * @since JDK 1.8
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
